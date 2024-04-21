8bd9786dc020387510ae55a89e6e316e27a4d64c
Tai Tran <ttran052899@gmail.com>
2024-04-21 15:55:46 -0700

    Added subdomain tests

--- unittest/scraper_tests.py
+++ unittest/scraper_tests.py
@@ -1,7 +1,8 @@
 import unittest
 from urllib.parse import urlparse
 from scraper import check_valid_domain
-
+from scraper import is_subdomain
+from scraper import count_subdomains
 
 class ScraperHelperTestCase(unittest.TestCase):
     def test_domain_validity(self):
@@ -14,6 +15,29 @@ class ScraperHelperTestCase(unittest.TestCase):
         self.assertEqual(False, check_valid_domain(parsed2))
         self.assertEqual(False, check_valid_domain(parsed3))
 
+    #robot testing
+
+    #uniqueness testing
+
+    #subdomain testing
+    def test_subdomain(self):
+        domain_bank = ("ics.uci.edu",
+                   "cs.uci.edu",
+                   "informatics.uci.edu",
+                    "stats.uci.edu"
+                    ".ics.uci.edu",
+                    ".cs.uci.edu",
+                     ".informatics.uci.edu",
+                     ".stats.uci.edu")
+        
+        self.assertTrue(is_subdomain("https://archive.ics.uci.edu/",domain_bank))
+        self.assertTrue(is_subdomain("https://ics.uci.edu/~dillenco/ics6d", domain_bank))
+    #subdomain counting
+        counter = 0
+        parsed_url = "https://archive.ics.uci.edu/"
+        parsed_url2 = "https://ics.uci.edu/~dillenco/ics6d"
+        self.assertEqual(1, count_subdomains(parsed_url, counter))
+        self.assertEqual(2, count_subdomains(parsed_url2, counter))
 
 if __name__ == '__main__':
     unittest.main()
