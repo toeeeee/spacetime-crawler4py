978568ea1198b0747db83f17f67ce20c1ab0a8d5
Anthony Nguyen <anthonyanthonyanthonynguyen@gmail.com>
2024-04-23 18:31:00 -0700

    u_pages
    Signed-off-by: Anthony Nguyen <anthonyanthonyanthonynguyen@gmail.com>

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ LONGEST_PAGE = ()  # ( format: page, number of words ) the page with greatest nu
 FREQ_DICT = {}  # dict of word-frequency pairs
 STOP_WORDS = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and",  "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing","don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours"] # list of words that will not be considered for the top 50 most common words
 SD_COUNT = {} # format: {"subdomain": count, ...}
-U_PAGES = {}  # Parsed urls
+U_PAGES = set()  # Parsed urls
 
 
 def scraper(url, resp) -> list:
