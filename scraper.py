e6750cea96dbfa99a9196c9692e9c4eafffff334
Anthony Nguyen <122919503+toeeeee@users.noreply.github.com>
2024-04-29 18:17:34 -0700

    db made once only

--- scraper.py
+++ scraper.py
@@ -7,18 +7,27 @@ from urllib.parse import urlparse, parse_qs
 
 
 # SCRAPER GLOBAL VARIABLES
+DB_MADE = False # changes to True after first runthrough
 LONGEST_PAGE = ()  # ( format: page, number of words ) the page with the greatest number of words
 FREQ_DICT = {}  # dict of word-frequency pairs
 STOP_WORDS = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and",  "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing","don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them","themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've","this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd","we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "which", "while","who", "whom", "why", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"] # list of words that will not be considered for the top 50 most common words
 SD_COUNT = {}  # format: {"subdomain": count, ...}
 U_PAGES = set()  # Parsed urls
 
+def make_db():
+    global DB_MADE
+    if not DB_MADE:
+        db = sqlite3.connect('hashes.db')  # implicitly create 'hashes.db' database if it doesn't exist, and create a connection to the db in the current working directory
+        cur = db.cursor()  # make a cursor to execute SQL statements and fetch results from SQL queries
+        cur.execute("CREATE TABLE pages(hash)")  # create the 'pages' table of hash values
+        cur.close()
+        DB_MADE = True
+    else:
+        return
+
 
 def scraper(url, resp) -> list:
-    db = sqlite3.connect('hashes.db')  # implicitly create 'hashes.db' database if it doesn't exist, and create a connection to the db in the current working directory
-    cur = db.cursor()  # make a cursor to execute SQL statements and fetch results from SQL queries
-    cur.execute("CREATE TABLE pages(hash)")  # create the 'pages' table of hash values
-    cur.close()
+    make_db()
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
